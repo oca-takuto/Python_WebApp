d5693e3a6956c483fc121153b7dbf0e1860dbfa9
自分のアカウント名 <>
2022-06-11 17:46:41 +0900

    request2

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return 'Response Data'
@@ -7,3 +9,11 @@ def index():
 @app.route('/another')
 def another():
     return 'Another Response'
+
+@app.route('/test_request')
+def test_request():
+    return f'test_request:{request.args.get("dummy")}'
+
+@app.route('/exercise_request')
+def exercise_request():
+    return f'Hello, {request.args.get("box")} .'
